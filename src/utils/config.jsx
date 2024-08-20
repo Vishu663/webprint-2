8dc1bc38cb013d1ec680c6741752ec4e76bdae91
Vishal Sanap <88238709+Vishu663@users.noreply.github.com>
2024-08-20 16:34:15 +0530

    Create config.jsx

---
+++ src/utils/config.jsx
@@ -0,0 +1,6 @@
+// config.jsx
+export const apiUrl1 = 'http://raspberrypi1.local/api';
+export const apiKey1 = '5D7CE556F6684CBAB47008A05E6743E5';
+
+export const apiUrl2 = 'http://raspberrypi2.local/api';
+export const apiKey2 = 'D51706341DA640C4AC655804841D4363';
